790eb68668d5500b3576d0151504ab5326ef3313
WinglessKing <128549723+WinglessKing@users.noreply.github.com>
2024-07-09 19:41:04 -0400

    Update bookShelf.js

--- milestone3files/bookShelf.js
+++ milestone3files/bookShelf.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    // Fetch and display books from bookshelf
     $.ajax({
         url: 'https://www.googleapis.com/books/v1/users/115677212204005988835/bookshelves/1001/volumes', // Correct URL syntax
         method: 'GET',
@@ -13,9 +12,9 @@ $(document).ready(function() {
     });
 
     function displayBookshelf(books) {
-        var bookshelfContainer = $("#bookshelf-container");
+        var bookshelfContainer = $("#bookshelfContainer");
         books.forEach(function(book) {
-            var bookItem = $('<div class="book-item" data-id="' + book.id + '"></div>');
+            var bookItem = $('<div class="bookCard" data-id="' + book.id + '"></div>');
             bookItem.append('<h3>' + book.volumeInfo.title + '</h3>');  // Use volumeInfo for title
             if (book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail) {
                 bookItem.append('<img src="' + book.volumeInfo.imageLinks.thumbnail + '" alt="' + book.volumeInfo.title + '">');
